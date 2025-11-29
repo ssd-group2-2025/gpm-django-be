1632a0f76d6a1f4f699b5407995b4e2369f282ab
matteobergantin <matteobergantin161@gmail.com>
2025-11-29 17:44:14 +0100

    Added custom serializer for User Registration
    Added Custom Serializer for user Registration in order to make "first_name" and "last_name" as mandatory fields in POST /api/v1/auth/registration

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,26 @@
-from rest_framework.serializers import ModelSerializer
+from dj_rest_auth.registration.serializers import RegisterSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from .models import Group
 
 class GroupSerializer(ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+class UserRegisterSerializer(RegisterSerializer):
+    # declare fields you want accepted
+    first_name = CharField(required=True, min_length=1, max_length=100)
+    last_name = CharField(required=True, min_length=1, max_length=100)
+
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['first_name'] = self.validated_data.get('first_name', '')
+        data['last_name'] = self.validated_data.get('last_name', '')
+        return data
+
+    def save(self, request):
+        user = super().save(request)
+        user.first_name = self.validated_data.get('first_name', '')
+        user.last_name = self.validated_data.get('last_name', '')
+        user.save()
+        return user
--- gpm_django_be/settings.py
+++ gpm_django_be/settings.py
@@ -252,3 +252,7 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_AUTH = {
+    "REGISTER_SERIALIZER": "core.serializers.UserRegisterSerializer",
+}
